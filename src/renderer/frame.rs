b4c99ede628a0517c0170095eaa79c9d8cf58aab
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-04-14 13:32:54 +0530

    Move draw_text function

--- src/renderer/frame.rs
+++ src/renderer/frame.rs
@@ -1,9 +1,6 @@
 use cairo::Context;
 use cairo::ImageSurface;
 
-use pango::FontDescription;
-use pangocairo::functions::{create_layout, show_layout};
-
 pub struct Frame {
     pub(crate) context: Option<Context>,
 }
@@ -17,26 +14,7 @@ impl Frame {
 
     pub fn get_context(&self) -> &Context {
         self.context.as_ref().unwrap()
-    }
-
-    pub fn draw_text(&self, text: &str, x: f64, y: f64, size: f64) {
-        let cr = self.get_context();
-
-        cr.move_to(x, y);
-
-        // Create Pango layout
-        let layout = create_layout(cr);
-
-        // Set font
-        let font_desc = FontDescription::from_string(&format!("JetBrainsMono {}", size));
-        layout.set_font_description(Some(&font_desc));
-
-        // Set text (could include markup)
-        layout.set_markup(text);
-
-        // Render text
-        show_layout(cr, &layout);
-    }
+    } 
 }
 
 impl Drop for Frame {
