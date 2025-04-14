a2e26105ba810f12ccca13792fb35ef4162cb858
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-04-14 14:34:32 +0530

    Example Circle tracing animation

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,23 @@ const SHOW_FFMPEG_OUTPUT: bool = false;
 fn main() {
     let clock = std::time::Instant::now();
     let mut renderer = Renderer::new(
-        7,            // duration in seconds
-        24,           // frames per second
+        15,           // duration in seconds
+        60,           // frames per second
         1800,         // width
         1000,         // height
         "output.mp4", // output file
     );
 
-    let mut animation = Animator::new(
+    let mut point_move = Animator::new(
         renderer.get_duration_parameter(1.0),
-        renderer.get_duration_parameter(5.0),
+        renderer.get_duration_parameter(4.0),
+        ezing::cubic_inout,
+        FinishAction::Stop,
+    );
+
+    let mut circle = Animator::new(
+        renderer.get_duration_parameter(4.0),
+        renderer.get_duration_parameter(12.0),
         ezing::cubic_inout,
         FinishAction::RepeatEnd,
     );
@@ -41,8 +48,7 @@ fn main() {
         graphics::draw_text(
             cr,
             "x",
-            width as f64 - 60.0,
-            center_y - 60.0,
+            (width as f64 - 60.0, center_y - 60.0),
             32.0,
             graphics::Color::White,
         );
@@ -50,35 +56,98 @@ fn main() {
         graphics::draw_text(
             cr,
             "y",
-            center_x + 30.0,
-            10.0,
+            (center_x + 30.0, 10.0),
             32.0,
             graphics::Color::White,
         );
 
+        let radius = 4.0;
+
         // draw a point which is initially at origin and then moves to (4, 3)
-        animation.draw(renderer.t(), |t| {
-            let x = center_x + 4.0 * spacing * t;
-            let y = center_y - 3.0 * spacing * t;
+        point_move.draw(renderer.t(), |t| {
+            let x = center_x + radius * spacing * t;
+            let y = center_y;
 
             graphics::draw_circle(
                 cr,
-                x,
-                y,
+                (x, y),
                 5.0,
                 10.0,
                 graphics::Color::Yellow,
                 Some(graphics::Color::Yellow),
             );
 
-            if t > 0.05 {
-                let text = format!(
-                    "({:.2}, {:.2})",
-                    (x - center_x) / 50.0,
-                    -(y - center_y) / 50.0
+            let text = format!(
+                "({:.2}, {:.2})",
+                (x - center_x) / 50.0,
+                (y - center_y) / 50.0
+            );
+
+            graphics::draw_text(
+                cr,
+                &text,
+                (x + 10.0, y - 70.0),
+                32.0,
+                graphics::Color::White,
+            );
+        });
+
+        // trace a circle
+        circle.draw(renderer.t(), |t| {
+            use std::f64::consts::*;
+
+            let angle = FRAC_PI_2 + PI * 2.0 * t;
+
+            let x = center_x + angle.sin() * spacing * radius;
+            let y = center_y + angle.cos() * spacing * radius;
+
+            if t != 1.0 {
+                graphics::draw_line(
+                    cr,
+                    (center_x, center_y),
+                    (x, y),
+                    2.0,
+                    graphics::Color::White,
                 );
-                graphics::draw_text(cr, &text, x + 10.0, y - 30.0, 32.0, graphics::Color::White);
             }
+
+            // tracing the circle
+            graphics::draw_arc(
+                cr,
+                (center_x, center_y),
+                FRAC_PI_2 - angle,
+                0.0,
+                radius * spacing,
+                4.0,
+                graphics::Color::White,
+            );
+
+            // point
+            graphics::draw_circle(
+                cr,
+                (x, y),
+                5.0,
+                10.0,
+                graphics::Color::Yellow,
+                Some(graphics::Color::Yellow),
+            );
+
+            let mut cy = -(y - center_y) / 50.0;
+
+            // fix `-0.00` in y coordinate
+            if cy > -0.001 && cy < 0.001 && cy.is_sign_negative() {
+                cy *= -1.0;
+            }
+
+            let text = format!("({:.2}, {cy:.2})", (x - center_x) / 50.0);
+
+            graphics::draw_text(
+                cr,
+                &text,
+                (x + 10.0, y - 70.0),
+                32.0,
+                graphics::Color::White,
+            );
         });
 
         // submit the frame to the renderer (blocking)
@@ -103,10 +172,8 @@ fn draw_coordinate_axes(cr: &cairo::Context, width: f64, height: f64) {
     // Draw x-axis
     graphics::draw_line(
         cr,
-        0.0,
-        center_y + 1.0,
-        width,
-        center_y + 1.0,
+        (0.0, center_y + 1.0),
+        (width, center_y + 1.0),
         2.0,
         graphics::Color::White,
     );
@@ -114,10 +181,8 @@ fn draw_coordinate_axes(cr: &cairo::Context, width: f64, height: f64) {
     // Draw y-axis
     graphics::draw_line(
         cr,
-        center_x,
-        0.0,
-        center_x,
-        height,
+        (center_x, 0.0),
+        (center_x, height),
         2.0,
         graphics::Color::White,
     );
@@ -135,11 +200,11 @@ fn draw_grid_lines(
 
     // Draw vertical lines
     for x in (0..(width as usize)).step_by(spacing as usize) {
-        graphics::draw_line(cr, x as f64, 0.0, x as f64, height, 1.0, color);
+        graphics::draw_line(cr, (x as f64, 0.0), (x as f64, height), 1.0, color);
     }
 
     // Draw horizontal lines
     for y in (0..(height as usize)).step_by(spacing as usize) {
-        graphics::draw_line(cr, 0.0, y as f64, width, y as f64, 1.0, color);
+        graphics::draw_line(cr, (0.0, y as f64), (width, y as f64), 1.0, color);
     }
 }
